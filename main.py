463d7120f642306777f952f3f92974c964005c7d
peko <yoyo.mepa@gmail.com>
2020-11-20 16:25:09 +0100

    init repo

---
+++ main.py
@@ -0,0 +1,11 @@
+import csv
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5555)
